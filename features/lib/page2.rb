87ee843bf73bb0016565427f26fec764ee2d68a5
Abhilash <abhilashshiyer@gmail.com>
2016-08-28 19:50:15 +0530

    removing older lib files

--- features/lib/page2.rb
+++
@@ -1,7 +0,0 @@
-class YML
-  # This method reads a YML file and returns the value for key elements
-  def read_yaml(key)
-    config = YAML.load_file("F:\\Cucumber\\standalone-cucumber-master\\features\\resources\\properties.yaml")
-    return config[key]
-  end
-end
